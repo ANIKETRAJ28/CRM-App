dc0c8252feb45f01a56b7d61490d6b42e843481c
Aniket <aniketraj28042003@gmail.com>
2024-07-05 16:30:19 +0530

    added axios config object

---
+++ src/config/axiosConfig.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosConfig = axios.create();
+
+axiosConfig.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axiosConfig.defaults.withCredentials = true,
+axiosConfig.defaults.timeout = import.meta.env.VITE_API_TIME_OUT;
+
+export default axiosConfig;
