2c8c812af68a00adb4343dd8dcc04fb482b50a55
Aniket <aniketraj28042003@gmail.com>
2024-08-07 16:36:18 +0530

    added changes

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosConfig = axios.create();
 
-axiosConfig.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axiosConfig.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:8000/crmapp/api/v1';
 // axiosConfig.defaults.timeout = import.meta.env.VITE_API_TIME_OUT;
 
 export default axiosConfig;
